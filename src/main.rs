216fbc9d4be1a33d257ff226cb5e342206ce9127
JerryLin083 <stabber5760@gmail.com>
2025-04-21 20:09:32 +0800

    replace postgres to tokio_postgres

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,84 @@
 use dotenv::dotenv;
 use openssl::ssl::{SslConnector, SslMethod, SslVerifyMode};
-use postgres::Config;
 use postgres_openssl::MakeTlsConnector;
+use tokio::{io::AsyncReadExt, net::TcpListener};
+use tokio_postgres::{Config, config};
 
 type Result<T> = std::result::Result<T, ()>;
 
-fn main() -> Result<()> {
-    //load dotenv
-    dotenv().map_err(|err| eprintln!("ERROR: {}", err))?;
+#[tokio::main]
+async fn main() -> Result<()> {
+    //TODO: error handle
 
-    //TODO: use tls connection
+    //load var from .enc
+    dotenv().map_err(|err| {
+        eprintln!("Error: {}", err);
+    })?;
+
+    //connection config
     let mut config = Config::new();
+
     config
         .host(&dotenv::var("HOST").unwrap_or(String::new()))
         .user(&dotenv::var("USER").unwrap_or(String::new()))
         .password(&dotenv::var("PASSWORD").unwrap_or(String::new()))
         .dbname(&dotenv::var("DBNAME").unwrap_or(String::new()))
-        .ssl_mode(postgres::config::SslMode::Require);
+        .ssl_mode(config::SslMode::Require);
 
     let mut builder = SslConnector::builder(SslMethod::tls()).map_err(|err| {
         eprintln!("Error: {}", err);
     })?;
+
     builder.set_verify(SslVerifyMode::NONE);
 
     let connector = MakeTlsConnector::new(builder.build());
 
-    let mut client = config.connect(connector).map_err(|err| {
+    let (client, connection) = config.connect(connector).await.map_err(|err| {
         eprintln!("Error: {}", err);
     })?;
 
-    let rows = client
-        .query(
-            "select first_name, last_name from customer order by first_name limit 10",
-            &[],
-        )
-        .map_err(|err| {
-            eprintln!("Error: {}", err);
-        })?;
+    //handle connection
+    tokio::spawn(async move {
+        if let Err(err) = connection.await {
+            eprintln!("Connecttion error: {}", err);
+        }
+    });
 
-    for row in rows {
-        let first_name: &str = row.get(0);
-        let last_name: &str = row.get(1);
+    //listen to socket connection
+    let listener = TcpListener::bind("127.0.0.1:8000").await.map_err(|err| {
+        eprintln!("Server error: {}", err);
+    })?;
 
-        println!("Customer name: {} {}", first_name, last_name);
+    loop {
+        if let Ok((mut stream, addr)) = listener.accept().await {
+            println!("socket connected");
+            client
+                .execute(
+                    "insert into connection(addr) values($1)",
+                    &[&addr.to_string()],
+                )
+                .await
+                .map_err(|err| {
+                    eprintln!("Insert error: {}", err);
+                })?;
+
+            tokio::spawn(async move {
+                let mut buf = vec![0; 4 * 1024];
+                match stream.read(&mut buf).await {
+                    Ok(0) => {
+                        println!("disconnection from {:?}", addr)
+                    }
+                    Ok(n) => {
+                        //TODO: hadnle client operation
+                        println!("Message from client: {:?}", &buf[..n]);
+                    }
+                    Err(err) => {
+                        eprintln!("Socket connection error: {}", err);
+                    }
+                };
+            })
+        } else {
+            continue;
+        };
     }
-
-    Ok(())
 }
